76eb3e78dfb2aa550133ff2bcd5f67ffdc4dbcc5
snowhamsters <snowhamsters@gmail.com>
2024-04-07 23:53:23 -0400

    partially working

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,18 +1,29 @@
 extends Control
 
-var totalMoney = 10000000;
-var profit = 1000000;
-var stock = 50.0;
+var totalMoney = 50000000000.0;
+var income = 2500000000.0;
+var stock = 105.0;
 var year = 2024;
-var environment = 0.0;
 
-var maxYear = 2100;
-var minEnvironment = -10.0;
+var environment = 90.0;
+var environmentRegen = 0.25;
+
+var prefix = [[1,"%10.2f"],[1e3,"%10.3fK"],
+		[1e6,"%10.3fM"],[1e9,"%10.3fB"],
+		[1e12,"%10.3fT"],[1e15,"%10.3fQa"]]
+
+var maxYear = 2050;
+var minEnvironment = 0.0;
+
+@onready var ChoiceScript = $"ChoiceScript"
 
 @onready var TotalMoneyValue = $"PlayerValues/TotalMoneyValue"
-@onready var ProfitValue = $"PlayerValues/ProfitValue"
+@onready var IncomeValue = $"PlayerValues/IncomeValue"
 @onready var StockValue = $"PlayerValues/StockValue"
 @onready var YearValue = $"GameValues/YearValue"
+
+@onready var EnvironmentHealthText = $"EnvironmentHealthText"
+@onready var EnvironmentHealthValue = $"EnvironmentHealthValue"
 
 @onready var Intro = $"Intro"
 @onready var IntroText = $"Intro/IntroText"
@@ -25,6 +36,7 @@ var minEnvironment = -10.0;
 
 @onready var Summary = $"Summary"
 @onready var SummaryText = $"Summary/SummaryText"
+@onready var SummaryImage = $"Summary/SummaryImage"
 
 @onready var Ending = $"Ending"
 @onready var EndingText = $"Ending/EndingText"
@@ -34,19 +46,54 @@ func _ready():
 	update_text_values()
 
 func _process(_delta):
-	pass
+	if Choice.visible == true:
+		if Input.is_action_just_pressed("SelectChoice1"):
+			process_choice(0)
+		if Input.is_action_just_pressed("SelectChoice2"):
+			process_choice(1)
+		if Input.is_action_just_pressed("SelectChoice3"):
+			process_choice(2)
+	else:
+		if Input.is_action_just_pressed("SelectContinue"):
+			if Intro.visible == true:
+				Intro.visible = false
+				generate_choice()
+			elif Summary.visible == true:
+				Summary.visible = false
+				year += 1
+				environment += environmentRegen
+				if (year >= maxYear or environment < minEnvironment):
+					trigger_ending()
+				else:
+					generate_choice()
+			elif Ending.visible == true:
+				get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func generate_choice():
-	# generate choice
-	# if (year == 1):
-	# else if (year == 2):
-	# else if (year == 3):
+	ChoiceText.text = ChoiceScript.choiceTextArray[year-2024]
+	ChoiceButton1.text = ChoiceScript.choiceOptionsArray[year-2024][0][0]
+	ChoiceButton2.text = ChoiceScript.choiceOptionsArray[year-2024][1][0]
+	ChoiceButton3.text = ChoiceScript.choiceOptionsArray[year-2024][2][0]
+	
 	update_text_values()
 	Choice.visible = true
 
 func process_choice(choiceNumber):
 	Choice.visible = false
-	# process choice
+	
+	var incomeChange = ChoiceScript.choiceOptionsArray[year-2024][choiceNumber][1]
+	var environmentChange = ChoiceScript.choiceOptionsArray[year-2024][choiceNumber][2]
+	var image = ChoiceScript.choiceOptionsArray[year-2024][choiceNumber][3]
+	
+	stock += ((incomeChange*income)/income)
+	income *= incomeChange
+	totalMoney += income
+	
+	environment += environmentChange
+	
+	if (image != ""):
+		SummaryImage.texture = load(image)
+	
 	update_text_values()
 	Summary.visible = true
 
@@ -55,24 +102,34 @@ func trigger_ending():
 	update_text_values()
 	Ending.visible = true
 
+func update_value_prefix(value):
+	var magnitude
+	for i in range(prefix.size()):
+		if (value >= prefix[i][0]):
+			magnitude = i
+	return prefix[magnitude][1] % [value/prefix[magnitude][0]]
+
 func update_text_values():
-	TotalMoneyValue.text = str(totalMoney)
-	ProfitValue.text = str(profit)
-	StockValue.text = str(stock)
+	TotalMoneyValue.text = update_value_prefix(totalMoney)
+	IncomeValue.text = update_value_prefix(income)
+	StockValue.text = update_value_prefix(stock)
 	YearValue.text = str(year)
+	
+	EnvironmentHealthValue.value = environment
 
 func _on_choice_button_1_pressed():
-	process_choice(1)
+	process_choice(0)
 
 func _on_choice_button_2_pressed():
-	process_choice(2)
+	process_choice(1)
 
 func _on_choice_button_3_pressed():
-	process_choice(3)
+	process_choice(2)
 
 func _on_continue_button_pressed():
 	Summary.visible = false
 	year += 1
+	environment += environmentRegen
 	if (year >= maxYear or environment < minEnvironment):
 		trigger_ending()
 	else:
---
+++ scripts/choice_script.gd
@@ -0,0 +1,172 @@
+extends Node
+
+var choice1Text = "This is an example problem 1 with choices"
+var choice1Option1 = ["option1", 0.9, 0.1, ""]
+var choice1Option2 = ["option2", 1.05, -1.0, "res://images/menu/fancy_cursor.png"]
+var choice1Option3 = ["option3", 1.2, -7.0, "res://images/menu/old_gauntlet.png"]
+
+var choice2Text = "This is an example problem 2 with choices"
+var choice2Option1 = ["option1", 10000, 0.1, ""]
+var choice2Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice2Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice3Text = "This is an example problem 3 with choices"
+var choice3Option1 = ["option1", 10000, 0.1, ""]
+var choice3Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice3Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice4Text = "This is an example problem 4 with choices"
+var choice4Option1 = ["option1", 10000, 0.1, ""]
+var choice4Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice4Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice5Text = "This is an example problem 5 with choices"
+var choice5Option1 = ["option1", 10000, 0.1, ""]
+var choice5Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice5Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice6Text = "This is an example problem 6 with choices"
+var choice6Option1 = ["option1", 10000, 0.1, ""]
+var choice6Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice6Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice7Text = "This is an example problem 7 with choices"
+var choice7Option1 = ["option1", 10000, 0.1, ""]
+var choice7Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice7Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice8Text = "This is an example problem 8 with choices"
+var choice8Option1 = ["option1", 10000, 0.1, ""]
+var choice8Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice8Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice9Text = "This is an example problem 9 with choices"
+var choice9Option1 = ["option1", 10000, 0.1, ""]
+var choice9Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice9Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice10Text = "This is an example problem 10 with choices"
+var choice10Option1 = ["option1", 10000, 0.1, ""]
+var choice10Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice10Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice11Text = "This is an example problem 11 with choices"
+var choice11Option1 = ["option1", 10000, 0.1, ""]
+var choice11Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice11Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice12Text = "This is an example problem 12 with choices"
+var choice12Option1 = ["option1", 10000, 0.1, ""]
+var choice12Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice12Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice13Text = "This is an example problem 13 with choices"
+var choice13Option1 = ["option1", 10000, 0.1, ""]
+var choice13Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice13Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice14Text = "This is an example problem 14 with choices"
+var choice14Option1 = ["option1", 10000, 0.1, ""]
+var choice14Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice14Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice15Text = "This is an example problem 15 with choices"
+var choice15Option1 = ["option1", 10000, 0.1, ""]
+var choice15Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice15Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice16Text = "This is an example problem 16 with choices"
+var choice16Option1 = ["option1", 10000, 0.1, ""]
+var choice16Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice16Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice17Text = "This is an example problem 17 with choices"
+var choice17Option1 = ["option1", 10000, 0.1, ""]
+var choice17Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice17Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice18Text = "This is an example problem 18 with choices"
+var choice18Option1 = ["option1", 10000, 0.1, ""]
+var choice18Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice18Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice19Text = "This is an example problem 19 with choices"
+var choice19Option1 = ["option1", 10000, 0.1, ""]
+var choice19Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice19Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice20Text = "This is an example problem 20 with choices"
+var choice20Option1 = ["option1", 10000, 0.1, ""]
+var choice20Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice20Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice21Text = "This is an example problem 21 with choices"
+var choice21Option1 = ["option1", 10000, 0.1, ""]
+var choice21Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice21Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice22Text = "This is an example problem 22 with choices"
+var choice22Option1 = ["option1", 10000, 0.1, ""]
+var choice22Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice22Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice23Text = "This is an example problem 23 with choices"
+var choice23Option1 = ["option1", 10000, 0.1, ""]
+var choice23Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice23Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice24Text = "This is an example problem 24 with choices"
+var choice24Option1 = ["option1", 10000, 0.1, ""]
+var choice24Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice24Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice25Text = "This is an example problem 25 with choices"
+var choice25Option1 = ["option1", 10000, 0.1, ""]
+var choice25Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice25Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choice26Text = "This is an example problem 26 with choices"
+var choice26Option1 = ["option1", 10000, 0.1, ""]
+var choice26Option2 = ["option2", 10000, -0.1, "res://images/menu/fancy_cursor.png"]
+var choice26Option3 = ["option3", 100000, -0.5, "res://images/menu/old_gauntlet.png"]
+
+var choiceTextArray = [
+		choice1Text, choice2Text, choice3Text,
+		choice4Text, choice5Text, choice6Text,
+		choice7Text, choice8Text, choice9Text,
+		choice10Text, choice11Text, choice12Text,
+		choice13Text, choice14Text, choice15Text,
+		choice16Text, choice17Text, choice18Text,
+		choice19Text, choice20Text, choice21Text,
+		choice22Text, choice23Text, choice24Text,
+		choice25Text, choice26Text,
+		]
+
+var choiceOptionsArray = [
+		[choice1Option1, choice1Option2, choice1Option3],
+		[choice2Option1, choice2Option2, choice2Option3],
+		[choice3Option1, choice3Option2, choice3Option3],
+		[choice4Option1, choice4Option2, choice4Option3],
+		[choice5Option1, choice5Option2, choice5Option3],
+		[choice6Option1, choice6Option2, choice6Option3],
+		[choice7Option1, choice7Option2, choice7Option3],
+		[choice8Option1, choice8Option2, choice8Option3],
+		[choice9Option1, choice9Option2, choice9Option3],
+		[choice10Option1, choice10Option2, choice10Option3],
+		[choice11Option1, choice11Option2, choice11Option3],
+		[choice12Option1, choice12Option2, choice12Option3],
+		[choice13Option1, choice13Option2, choice13Option3],
+		[choice14Option1, choice14Option2, choice14Option3],
+		[choice15Option1, choice15Option2, choice15Option3],
+		[choice16Option1, choice16Option2, choice16Option3],
+		[choice17Option1, choice17Option2, choice17Option3],
+		[choice18Option1, choice18Option2, choice18Option3],
+		[choice19Option1, choice19Option2, choice19Option3],
+		[choice20Option1, choice20Option2, choice20Option3],
+		[choice21Option1, choice21Option2, choice21Option3],
+		[choice22Option1, choice22Option2, choice22Option3],
+		[choice23Option1, choice23Option2, choice23Option3],
+		[choice24Option1, choice24Option2, choice24Option3],
+		[choice25Option1, choice25Option2, choice25Option3],
+		[choice26Option1, choice26Option2, choice26Option3],
+		]
