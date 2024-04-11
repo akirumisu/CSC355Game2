eb87962105da7f5bbab3a7b1748c190d75d5f9c3
snowhamsters <snowhamsters@gmail.com>
2024-04-11 14:38:40 -0400

    a little more

--- scripts/choice_script.gd
+++ scripts/choice_script.gd
@@ -16,30 +16,31 @@ var summary2Option1 = "The savings from using LED lights are incredible. The cos
 var summary2Option2 = "You decide to keep the lights how they are. Changing all the lights sounds too complicated, spending less on electricity is not a priority it seems."
 var summary2Option3 = "The savings from using LED lights are incredible. The cost of lighting is 400% cheaper for all buildings now. Lighting is only a tiny part of the companies expenses, so the amount you save equally tiny. Thankfully, having a smaller electricity bill helps out the environment."
 
-var choice3Text = "This is an example problem 3 with choices"
-var choice3Option1 = ["option1", 1.0, 0.0, 1.0, ""]
-var choice3Option2 = ["option2", 1.0, 0.0, 1.0, ""]
-var choice3Option3 = ["option3", 1.0, 0.0, 1.0, ""]
-var summary3Option1 = ""
-var summary3Option2 = ""
-var summary3Option3 = ""
+# https://en.wikipedia.org/wiki/Controversies_of_Nestl%C3%A9
+var choice3Text = "You have received a tip that other companies are buying lakes to start their own bottled water products. What do you do?"
+var choice3Option1 = ["Pump water from a lake in the US", 1.1, -2.0, 0.9, ""]
+var choice3Option2 = ["Buy cheap lakes in a 3rd world country", 1.3, -5.0, 0.9, ""]
+var choice3Option3 = ["Drill underground reservoirs for water", 1.3, -10.0, 0.8, ""]
+var summary3Option1 = "You decide to pump water from a lake in the US. Most of your customer are from the US and approve this action to provide jobs. Some oppose it, but as long as you limit water pumping, the population should be okay with it."
+var summary3Option2 = "You buy cheap lakes from a 3rd world country. Instead of selling water to citizens in the country, you decide to market your water to US customers. This creates immensive profits. The citizens from the 3rd world country are harmed by this practice."
+var summary3Option3 = "You drill underground reservoirs for water. It costs a lot of money to get started, but thankfully you can market this as the freshest water in the world. It works, and people are willing to pay extra for your bottled water. It turns out, these drills used to reach the underground reservoir emit a lot of CO2, along with destroying the environment."
 
 # https://afdc.energy.gov/vehicles/natural-gas
 var choice4Text = "The company is expanding greatly and needs a new flight of semi-trucks to deliver food products. What kind of fleet should you buy?"
 var choice4Option1 = ["Electric semi-trucks", 1.1, 1.0, 0.85, ""]
-var choice4Option2 = ["Compressed natural gas trucks", 1.2, -5, 0.95, ""]
-var choice4Option3 = ["Traditional semi-trucks", 1.3, -10.0, 0.9, ""]
+var choice4Option2 = ["Compressed natural gas trucks", 1.2, -10, 0.95, ""]
+var choice4Option3 = ["Traditional semi-trucks", 1.3, -15.0, 0.9, ""]
 var summary4Option1 = "You replace the fleet of semi-trucks with... electric semi-trucks. They were cheaper through a government tax write-off. The lower mileage until stopping for a recharge slightly lowered your production, but the environment thanks you."
 var summary4Option2 = "You replace the fleet of semi-trucks with... compressed natural gas trucks. There are some refueling and mechanic problems with them, but at least your company got a nice tax write-off. The price of CNG is lower than diesel fuel, and the gas emits slightly less CO2 than diesel."
 var summary4Option3 = "You replace the fleet of semi-trucks with... the same semi-trucks again. The other choices sounded too fancy and complicated. After all, other companies are still sticking with traditional semi-trucks as long as they can."
 
-var choice5Text = "This is an example problem 5 with choices"
-var choice5Option1 = ["option1", 1.0, 0.0, 1.0, ""]
-var choice5Option2 = ["option2", 1.0, 0.0, 1.0, ""]
-var choice5Option3 = ["option3", 1.0, -20.0, 1.0, ""]
-var summary5Option1 = ""
-var summary5Option2 = ""
-var summary5Option3 = ""
+var choice5Text = "You recieve another tip involving bottled water. Your competitors are planning on increasing their bottled water production along with aggressive marketing for a larger market share. What do you do?"
+var choice5Option1 = ["Increase production and marketing by 50%", 1.2, -5.0, 0.8, ""]
+var choice5Option2 = ["Increase production and marketing by 100%", 1.3, -10.0, 0.6, ""]
+var choice5Option3 = ["Leave the bottled water industry", 0.8, 1.0, 1.0, ""]
+var summary5Option1 = "You increase production and marketing by 50%. This move results in the company staying competitive in the bottled water market. The environment is not happy about it, but your stockholders enjoy it."
+var summary5Option2 = "You double down and increase production and marketing by 100%. As a result, your market share in bottled water increased even after competitiors marketing. Unfortunately, you get into a little bit of legal trouble as the population notices your antics. Perhaps you increased production too much?"
+var summary5Option3 = "You leave the bottled water industry. Hurting the environment just to stay competitive is not worth it."
 
 var choice6Text = "This is an example problem 6 with choices"
 var choice6Option1 = ["option1", 1.0, 0.0, 1.0, ""]
@@ -113,14 +114,6 @@ var summary14Option1 = ""
 var summary14Option2 = ""
 var summary14Option3 = ""
 
-var choice15Text = "The population is becoming increasingly worried about climate change. People have started to boycott your company, along with other companies emitting high CO2."
-var choice15Option1 = ["Cut emissions and advertise about it", 1.0, 0.1, 0.9, ""]
-var choice15Option2 = ["Cut emissions", 0.95, 0.1, 1.0, ""]
-var choice15Option3 = ["Do nothing", 1.0, -1.0, 1.0, ""]
-var summary15Option1 = ""
-var summary15Option2 = ""
-var summary15Option3 = ""
-
 # https://en.wikipedia.org/wiki/ExxonMobil_climate_change_denial#Lobbying_against_emissions_regulations
 var choice16Text = "Congress have cracked down on climate change to appease the population. If the deal goes through, your company will have to lower CO2 emissions or take hefty fines."
 var choice16Option1 = ["Slightly lower production and emissions", 0.9, 0.2, 1.0, ""]
@@ -129,6 +122,14 @@ var choice16Option3 = ["Lobby against emissions regulations", 1.1, -5.0, 0.95, "
 var summary16Option1 = "You slightly lower production and emissions. Your company does not have to pay a fine."
 var summary16Option2 = "You do nothing. The deal went through Congress, and your company was forced to pay a fine. Thankfully the profit from production mostly offset the fines."
 var summary16Option3 = "You lobby against emissions regulations. It was... successful. It may be bad for the environment, but your stockholders are happy."
+
+var choice15Text = "The population is still becoming increasingly worried about climate change. People have started to boycott your company, along with other companies who do not have a net zero carbon plan."
+var choice15Option1 = ["Introduce a net zero plan... and stick to it", 0.9, 1.0, 0.5, ""]
+var choice15Option2 = ["Introduce a net zero plan... and half stick to it", 0.95, -10.0, 0.8, ""]
+var choice15Option3 = ["Do nothing", 0.6, -20.0, 1.0, ""]
+var summary15Option1 = "You introduce an achievable net zero plan. Your company plans to stick to it and avoid any future contreversies. Unfortunately, the environment has already been damaged over the years from your actions as well as other companies. But at least this way, you can profit from future consumers in a changing world."
+var summary15Option2 = "You introduce an unachievable net zero plan. Your company would have to completely restructure the way you process food, and you deem that unattainable. Thankully, the public doesn't know that the company doesn't plan on meeting it's net zero goals. In the future, most people will forget the promises your company made."
+var summary15Option3 = "You do nothing, hoping that this will blow over. It doesn't. Other companies introduce net zero plans, while yours remains boycotted and targetted more than ever."
 
 # I AM STOPPING HERE AT YEAR 2040, INSTEAD OF THE YEAR 2050 AS ORIGINALLY PLANNED.
 # https://i.kym-cdn.com/photos/images/newsfeed/001/983/888/25d.png
